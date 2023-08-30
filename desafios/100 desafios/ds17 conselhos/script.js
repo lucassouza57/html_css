f996069c9f53ffab66c09b53d031281e54efef9d
lucassouza57 <lucas78957@gmail.com>
2023-08-30 18:25:45 -0300

    ultimos desfios

--- desafios/100 desafios/ds17 conselhos/script.js
+++ desafios/100 desafios/ds17 conselhos/script.js
@@ -1,16 +1,16 @@
-const generateBtn = document.getElementById("generateBtn")
-const adviceContainer = document.getElementById("adviceContainer")
+const generateBtn = document.getElementById("generateBtn");
+const adviceContainer = document.getElementById("adviceContainer");
 
 generateBtn.addEventListener("click", () => {
-    fetch("https://api.adviceslip.com/advice?lang=pt")
+    fetch("https://api.quotable.io/random")
         .then(response => response.json())
         .then(data => {
-            const advice = data.slip.advice;
-            adviceContainer.innerHTML = `<p>${advice}</p>`
+            const advice = data.content;
+            adviceContainer.innerHTML = `<p>${advice}</p>`;
         })
         .catch(error => {
-            console.error("Erro ao obter conselho:", error)
-            adviceContainer.innerHTML = "<p>There was an error getting advice.</p>"
+            console.error("Erro ao obter conselho:", error);
+            adviceContainer.innerHTML = "<p>Ocorreu um erro ao obter um conselho.</p>";
             adviceContainer.style.color = 'red'
             adviceContainer.style.fontWeight = 'bold'
         })
