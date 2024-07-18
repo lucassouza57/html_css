27938498f867a1c2d3c4123772d13ab02c2c3d7f
lucas souza <129347854+lucassouza57@users.noreply.github.com>
2024-07-18 20:41:46 -0300

    aulas de banco de dados

---
+++ Banco de dados/aula01.sql
@@ -0,0 +1,8 @@
+insert into pessoas
+(nome, nacimento, sexo, peso, altura)
+values
+('Gustavo', '1997-02-22', 'M', '99.8', '1.90'),
+('Gustavo', '1996-02-22', 'M', '88.8', '1.88'),
+('Ana', '1992-08-22', 'F', '70.8', '1.50');
+
+select * from pessoas;
