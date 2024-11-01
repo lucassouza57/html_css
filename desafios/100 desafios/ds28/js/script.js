11f8082e122e018267b69ade4e1d59787aba14b8
lucas souza <129347854+lucassouza57@users.noreply.github.com>
2024-11-01 20:43:47 -0300

    add funções auxiliares e deixando o codigo mais legivel

--- desafios/100 desafios/ds28/js/script.js
+++ desafios/100 desafios/ds28/js/script.js
@@ -1,38 +1,63 @@
+// Funções auxiliares
+const seleciona = (elemento) => document.querySelector(elemento);
+const selecionaTodos = (elemento) => document.querySelectorAll(elemento);
+
+const abrirModal = () => {
+    seleciona('.pizzaWindowArea').style.opacity = 0;
+    seleciona('.pizzaWindowArea').style.display = 'flex';
+    setTimeout(() => {
+        seleciona('.pizzaWindowArea').style.opacity = 1;
+    }, 150);
+}
+
+const fecharModal = () => {
+    seleciona('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(() => {
+        seleciona('.pizzaWindowArea').style.display = 'none';
+    }, 500);
+}
+
+const botoesFechar = () => {
+    // Botões para fechar modal
+    selecionaTodos('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
+        item.addEventListener('click', fecharModal);
+    });
+}
+
+// Preencher dados de cada pizza
+const preencherDadosDasPizzas = (pizzaItem, item) => {
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+}
+
+// Inicializar a função para fechar o modal nos botões
+botoesFechar();
+
+// Mapear pizzaJson para gerar listas de pizzas
 pizzaJson.map((item, index) => {
     // Clonar o modelo de item de pizza
     let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
 
     // Adicionar o item clonado na área de pizzas
-    document.querySelector('.pizza-area').append(pizzaItem);
+    seleciona('.pizza-area').append(pizzaItem);
 
     // Preencher dados de cada pizza
-    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
-    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
-    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
-    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    preencherDadosDasPizzas(pizzaItem, item);
 
     // Quando clicar na pizza
     pizzaItem.querySelector('.pizza-item a').addEventListener('click', (e) => {
         e.preventDefault();
         console.log('Clicou na pizza');
 
-     // Abrir janela modal
-        document.querySelector('.pizzaWindowArea').style.display = 'flex'
+        // Abrir janela modal
+        abrirModal();
 
-    // Preenchimento dos dados da janela modal
-        document.querySelector('.pizzaInfo h1').innerHTML = item.name
+        // Preenchimento dos dados da janela modal
+        document.querySelector('.pizzaInfo h1').innerHTML = item.name;
         document.querySelector('.pizzaBig img').src = item.img;
-        document.querySelector('.pizzaInfo--desc').innerHTML = item.description
-        document.querySelector('.pizzaInfo--actualPrice').innerHTML = `R$ ${item.price.toFixed(2)}`
+        document.querySelector('.pizzaInfo--desc').innerHTML = item.description;
+        document.querySelector('.pizzaInfo--actualPrice').innerHTML = `R$ ${item.price.toFixed(2)}`;
     });
-
-    // Fechar janela modal
-    document.querySelector('.pizzaInfo--cancelButton').addEventListener('click', () => {
-        document.querySelector('.pizzaWindowArea').style.display = 'none'
-    })
-
-    // Fechar janela modal mobile
-    document.querySelector('.pizzaInfo--cancelMobileButton').addEventListener('click', () => {
-        document.querySelector('.pizzaWindowArea').style.display = 'none'
-    })
 });
