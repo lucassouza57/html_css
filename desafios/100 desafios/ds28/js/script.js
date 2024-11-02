ad9a9bf8b9c406b5e606458f16a7f4702976aa3c
lucas souza <129347854+lucassouza57@users.noreply.github.com>
2024-11-02 17:11:48 -0300

    aqui eu configurei para aparecer os preços e tamanhos quando abrir o modal

--- desafios/100 desafios/ds28/js/script.js
+++ desafios/100 desafios/ds28/js/script.js
@@ -1,6 +1,25 @@
+// Variável modalKey será global
+let modalKey = 0;
+
+// Variável para controlar a quantidade inicial de pizzas no modal
+let quantidadePizzas = 1;
+
+// Carrinho
+let cart = [];
+
 // Funções auxiliares
 const seleciona = (elemento) => document.querySelector(elemento);
 const selecionaTodos = (elemento) => document.querySelectorAll(elemento);
+
+const formatoReal = (valor) => {
+    return valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+}
+
+const formatoMonetario = (valor) => {
+    if (valor) {
+        return valor.toFixed(2);
+    }
+}
 
 const abrirModal = () => {
     seleciona('.pizzaWindowArea').style.opacity = 0;
@@ -25,12 +44,81 @@ const botoesFechar = () => {
 }
 
 // Preencher dados de cada pizza
-const preencherDadosDasPizzas = (pizzaItem, item) => {
+const preencherDadosDasPizzas = (pizzaItem, item, index) => {
+    pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
-    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price[0].toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
 }
+
+// Preenchimento dos dados da janela modal
+const preencheDadosModal = (item) => {
+    seleciona('.pizzaInfo h1').innerHTML = item.name;
+    seleciona('.pizzaBig img').src = item.img;
+    seleciona('.pizzaInfo--desc').innerHTML = item.description;
+    seleciona('.pizzaInfo--actualPrice').innerHTML = formatoReal(item.price[2]);
+}
+
+// Função para pegar o índice da pizza clicada
+const pegarKey = (e) => {
+    let key = e.target.closest('.pizza-item').getAttribute('data-key');
+    console.log('Clicou na pizza', key);
+    console.log(pizzaJson[key]);
+
+    // Garantir que a quantidade inicial da pizza seja 1
+    quantidadePizzas = 1;
+
+    // Manter a informação de qual pizza foi clicada
+    modalKey = key;
+
+    return key;
+}
+
+// Preencher tamanhos
+const preencherTamanhos = (key) => {
+    // Tira a seleção de tamanho atual e seleciona o tamanho grande
+    seleciona('.pizzaInfo--size.selected').classList.remove('selected');
+
+    // Seleciona todos os tamanhos
+    selecionaTodos('.pizzaInfo--size').forEach((size, sizeIndex) => {
+        // Seleciona o tamanho grande
+        (sizeIndex == 2) ? size.classList.add('selected') : '';
+        size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+    });
+}
+
+// Escolher o tamanho e atualizar o preço
+const escolherTamanhoPreco = (key) => {
+    // Ações nos botões de tamanho
+    selecionaTodos('.pizzaInfo--size').forEach((size, sizeIndex) => {
+        size.addEventListener('click', () => {
+            // Tirar a seleção de tamanho atual e selecionar o tamanho escolhido
+            seleciona('.pizzaInfo--size.selected').classList.remove('selected');
+            size.classList.add('selected');
+
+            // Atualizar o preço de acordo com o tamanho
+            seleciona('.pizzaInfo--actualPrice').innerHTML = formatoReal(pizzaJson[key].price[sizeIndex]);
+        });
+    });
+}
+
+// Mudar a quantidade de pizzas
+const mudarQuantidade = () => {
+    // Ações nos botões de + e - na janela modal
+    seleciona('.pizzaInfo--qtmais').addEventListener('click', () => {
+        quantidadePizzas++;
+        seleciona('.pizzaInfo--qt').innerHTML = quantidadePizzas;
+    });
+    seleciona('.pizzaInfo--qtmenos').addEventListener('click', () => {
+        if (quantidadePizzas > 1) {
+            quantidadePizzas--;
+            seleciona('.pizzaInfo--qt').innerHTML = quantidadePizzas;
+        }
+    });
+}
+
+
 
 // Inicializar a função para fechar o modal nos botões
 botoesFechar();
@@ -44,20 +132,29 @@ pizzaJson.map((item, index) => {
     seleciona('.pizza-area').append(pizzaItem);
 
     // Preencher dados de cada pizza
-    preencherDadosDasPizzas(pizzaItem, item);
+    preencherDadosDasPizzas(pizzaItem, item, index);
 
     // Quando clicar na pizza
     pizzaItem.querySelector('.pizza-item a').addEventListener('click', (e) => {
         e.preventDefault();
-        console.log('Clicou na pizza');
+        let chave = pegarKey(e);
 
-        // Abrir janela modal
+        // Abrir janela modal e preencher com os dados da pizza clicada
         abrirModal();
 
-        // Preenchimento dos dados da janela modal
-        document.querySelector('.pizzaInfo h1').innerHTML = item.name;
-        document.querySelector('.pizzaBig img').src = item.img;
-        document.querySelector('.pizzaInfo--desc').innerHTML = item.description;
-        document.querySelector('.pizzaInfo--actualPrice').innerHTML = `R$ ${item.price.toFixed(2)}`;
+        // Preenchimento dos dados
+        preencheDadosModal(item);
+
+        // Pegar tamanho selecionado
+        preencherTamanhos(chave);
+
+        // Definir quantidade inicial como 1
+        seleciona('.pizzaInfo--qt').innerHTML = quantidadePizzas;
+
+        // Selecionar o tamanho e preço com o clique no botão
+        escolherTamanhoPreco(chave);
+
+        // Inicializar controle de quantidade
+        mudarQuantidade();
     });
 });
--- desafios/100 desafios/ds28/pizzas.js
+++ desafios/100 desafios/ds28/pizzas.js
@@ -1,10 +1,10 @@
 let pizzaJson = [
-    {id:1, name:'Mussarela', img:'images/pizza.png', price:25.00, sizes:['4 fatias', '6 fatias', '8 fatias'], description:'Molho de tomate, camada dupla de mussarela e orégano'},
-    {id:2, name:'Calabresa', img:'images/pizza2.png', price:26.00, sizes:['4 fatias', '6 fatias', '8 fatias'], description:'Molho de tomate, mussarela, calabresa fatiada, cebola e orégano'},
-    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:28.00, sizes:['4 fatias', '6 fatias', '8 fatias'], description:'Molho de tomate, camadas de mussarela, provolone, parmessão, gorgonzola e orégano'},
-    {id:4, name:'Brasileira', img:'images/pizza4.png', price:30.00, sizes:['4 fatias', '6 fatias', '8 fatias'], description:'Molho de tomate, mussarela, calabresa picada, palmito, champignon, azeitonas e orégano'},
-    {id:5, name:'Portuguesa', img:'images/pizza5.png', price:29.00, sizes:['4 fatias', '6 fatias', '8 fatias'], description:'Molho de tomate, mussarela, presunto, ovos, cebolas, pimentão, azeitona  e orégano'},
-    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:35.00, sizes:['4 fatias', '6 fatias', '8 fatias'], description:'Molho de tomate, mussarela, carne de sol, tomates em cubos, coentro, cebola, azeitona, catupiry e orégano'},
-    {id:7, name:'Banana com canela', img:'images/pizza7.png', price:32.00, sizes:['4 fatias', '6 fatias', '8 fatias'], description:'Mussarela, banana, canela e açúcar'},
-    {id:8, name:'Chocolate com morango', img:'images/pizza8.png', price:35.00, sizes:['4 fatias', '6 fatias', '8 fatias'], description:'Creme de leite, lascas de chocolate e morangos'}
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:[20.00, 25.00, 30.00], sizes:['4 fatias', '6 fatias', '8 fatias'], description:'Molho de tomate, camada dupla de mussarela e orégano'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:[20.00, 25.00, 30.00], sizes:['4 fatias', '6 fatias', '8 fatias'], description:'Molho de tomate, mussarela, calabresa fatiada, cebola e orégano'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:[23.00, 28.00, 33.00], sizes:['4 fatias', '6 fatias', '8 fatias'], description:'Molho de tomate, camadas de mussarela, provolone, parmesão, gorgonzola e orégano'},
+    {id:4, name:'Brasileira', img:'images/pizza4.png', price:[25.00, 30.00, 35.00], sizes:['4 fatias', '6 fatias', '8 fatias'], description:'Molho de tomate, mussarela, calabresa picada, palmito, champignon, azeitonas e orégano'},
+    {id:5, name:'Portuguesa', img:'images/pizza5.png', price:[22.00, 27.00, 32.00], sizes:['4 fatias', '6 fatias', '8 fatias'], description:'Molho de tomate, mussarela, presunto, ovos, cebolas, pimentão, azeitona e orégano'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:[22.00, 27.00, 32.00], sizes:['4 fatias', '6 fatias', '8 fatias'], description:'Molho de tomate, mussarela, carne de sol, tomates em cubos, coentro, cebola, azeitona, catupiry e orégano'},
+    {id:7, name:'Banana com canela', img:'images/pizza7.png', price:[25.00, 30.00, 35.00], sizes:['4 fatias', '6 fatias', '8 fatias'], description:'Mussarela, banana, canela e açúcar'},
+    {id:8, name:'Chocolate com morango', img:'images/pizza8.png', price:[23.00, 28.00, 33.00], sizes:['4 fatias', '6 fatias', '8 fatias'], description:'Creme de leite, lascas de chocolate e morangos'}
 ];
